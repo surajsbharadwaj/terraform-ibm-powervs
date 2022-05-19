3cb798fcb19773740c7bebfbc95cc6b22efa7139
Suraj Bharadwaj <suraj.bharadwaj@ibm.com>
2022-05-19 11:48:19 +0200

    variable desc

--- modules/pvs-instance-sap-init-sles/variables.tf
+++ modules/pvs-instance-sap-init-sles/variables.tf
@@ -1,47 +1,50 @@
 variable "bastion_public_ip" {
-    description = "Public IP of Bastion Host"
+  description = "Public IP of Bastion Host"
+  type        = string
 }
 
 variable "host_private_ip" {
-    description = "Private IP of Netweaver/HANA Host reachable from bastion"
+  description = "Private IP of Netweaver/HANA Host reachable from bastion"
+  type        = string
 }
 
 variable "ssh_private_key" {
-    type        = string
-    description = "Private Key to confgure Instance, Will not be uploaded to server" 
+  description = "Private Key to confgure Instance, Will not be uploaded to server" 
+  type        = string
 }
 
 variable "vpc_address_prefix" {
-    description = "Existing Address prefix of VPC to reach the HANA/NW node from machine on vpc"
+  description = "Existing Address prefix of VPC to reach the HANA/NW node from machine on vpc"
+  type        = string
 }
 
 variable "pvs_bastion_snat_config" {
-    type        = map
-    description = "SNAT configuration if required on HANA/nw node to reach public internet via the Bastion host on Power Server"
-    default     = {
+  description = "SNAT configuration if required on HANA/nw node to reach public internet via the Bastion host on Power Server"
+  type        = map
+  default     = {
                     required = false
                     pvs_bastion_private_ip = ""
-                  }
+                }
 }
 
 variable "vpc_bastion_proxy_config" {
-    type        = map
-    description = "SQUID configuration if required on HANA/nw node to reach public internet via the Bastion host on VSI running SQUID server"
-    default     = {
+  description = "SQUID configuration if required on HANA/nw node to reach public internet via the Bastion host on VSI running SQUID server"
+  type        = map
+  default     = {
                     required = false
                     vpc_bastion_private_ip = ""
                     no_proxy_ips           = ""
-                  }
+                }
 }
 
 variable "suse_activation" {
-    type        = map
-    description = "Suse activation email and code to register for suse"
-    default     = {
+  description = "Suse activation email and code to register for suse"
+  type        = map
+  default     = {
                     required         = false
                     activation_email = ""
                     activation_code  = ""
-                  }
+                }
 }
 
 variable "pvs_instance_storage_config" {
@@ -53,10 +56,10 @@ variable "pvs_instance_storage_config" {
                     disks_size = ""
                     counts     = ""
                     wwns       = ""
-  }
+                }
 }
 
 variable "sap_solution" {
-    type        = string
-    description = "To Execute Playbooks for Hana or Netweaver. Value can be either HANA OR NETWEAVER"
+  description = "To Execute Playbooks for Hana or Netweaver. Value can be either HANA OR NETWEAVER"
+  type        = string
 }
