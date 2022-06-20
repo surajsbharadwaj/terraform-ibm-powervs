ffc4486ba2dd48fa39c2274a5fcd310f60f20a09
Suraj Bharadwaj <suraj.bharadwaj@ibm.com>
2022-06-20 23:30:09 +0200

    new

--- examples/power-sap-instance-hana/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "BASTION_PUBLIC_IP" {
-  description = "Public IP of Provided Bastion/JumpServer Host"
-  value      = module.hana.bastion_public_ip
-}
-
-output "PVS_HANA_IPS" {
-  description = "All private IPS of HANA instance"
-  value      = module.hana.pvs_instance_ips
-}
